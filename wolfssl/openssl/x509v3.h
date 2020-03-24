926c2232e30193f9189559491f0a7b50eea33d72
TaigaKoma <taiga416@ertl.jp>
2020-03-24 13:40:40 +0900

    add openssl macro

--- wolfssl/openssl/x509v3.h
+++ wolfssl/openssl/x509v3.h
@@ -1,2 +1,13 @@
 /* x509v3.h for openssl */
-
+# define X509_PURPOSE_SSL_CLIENT       	1
+# define X509_PURPOSE_SSL_SERVER       	2
+# define X509_PURPOSE_NS_SSL_SERVER   	3
+# define X509_PURPOSE_SMIME_SIGN       	4
+# define X509_PURPOSE_SMIME_ENCRYPT   	5
+# define X509_PURPOSE_CRL_SIGN          	6
+# define X509_PURPOSE_ANY                	7
+# define X509_PURPOSE_OCSP_HELPER      	8
+# define X509_PURPOSE_TIMESTAMP_SIGN  	9
+ 
+# define X509_PURPOSE_MIN                	1
+# define X509_PURPOSE_MAX                	9
